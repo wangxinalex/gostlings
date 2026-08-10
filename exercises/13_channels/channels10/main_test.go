f74550d5bb7927043306f25c43af5708cb55b1f1
Xin Wang <wangxinalex@gmail.com>
2026-08-10 23:12:26 +0800

    test: stabilize channels10 cancellation test (#18)

--- exercises/13_channels/channels10/main_test.go
+++ exercises/13_channels/channels10/main_test.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"reflect"
+	"runtime"
 	"testing"
 	"time"
 )
 
 func TestProduceStopsWhenCancellationArrivesBeforeReceive(t *testing.T) {
+	previousProcs := runtime.GOMAXPROCS(1)
+	defer runtime.GOMAXPROCS(previousProcs)
+
 	stop := make(chan struct{})
 	out := produce(stop)
+	runtime.Gosched() // Let the producer reach its blocking send.
 	close(stop)
+	runtime.Gosched() // Let the producer observe cancellation before receiving.
 
 	select {
 	case _, ok := <-out:
